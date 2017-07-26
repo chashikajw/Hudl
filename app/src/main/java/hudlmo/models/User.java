6bce50456e2219df0a182e8dfb96a68fba2b256d
chashikajw <chashikajw007@gmail.com>
2017-07-27 02:12:26 +0530

    modify the User class

--- app/src/main/java/hudlmo/models/User.java
+++ app/src/main/java/hudlmo/models/User.java
@@ -1,8 +1,53 @@
 package hudlmo.models;
 
+import java.io.Serializable;
+
 /**
  * Created by chashika on 7/26/17.
  */
 
-public class User {
+public class User implements Serializable {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
