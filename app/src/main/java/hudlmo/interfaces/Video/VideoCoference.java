a8dabc71a46dd0060f5d14831a2f1725ca9317f6
chashikajw <chashikajw007@gmail.com>
2017-12-03 14:09:33 +0530

    modified manisfest for activities

---
+++ app/src/main/java/hudlmo/interfaces/Video/VideoCoference.java
@@ -0,0 +1,15 @@
+package hudlmo.interfaces.Video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hudlmo.interfaces.loginpage.R;
+
+public class VideoCoference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_coference);
+    }
+}
