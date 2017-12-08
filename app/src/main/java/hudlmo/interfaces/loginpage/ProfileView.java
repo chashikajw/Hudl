b1349fe22ac4e20635e5ff9471f3209321c2ca63
chashikajw <chashikajw007@gmail.com>
2017-12-08 17:01:19 +0530

    add users profile view

---
+++ app/src/main/java/hudlmo/interfaces/loginpage/ProfileView.java
@@ -0,0 +1,13 @@
+package hudlmo.interfaces.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view);
+    }
+}
---
+++ app/src/main/java/hudlmo/models/UsersActivity.java
@@ -0,0 +1,15 @@
+package hudlmo.models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hudlmo.interfaces.loginpage.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
